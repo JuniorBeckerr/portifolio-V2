3e7667846f79e95d79cb3ceec609f9a45cc865ca
JuniorrBecker <arnaldo.becker@outlook.com>
2024-07-03 00:48:59 -0300

    Corrige caminho do arquivo MobileNav.css e verifica build local

--- src/components/Navbar/MobileNav/MobileNav.jsx
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -2,42 +2,61 @@ import React from "react";
 import "./MobileNav.css";
 
 const MobileNav = ({ isOpen, toggleMenu }) => {
-  
+
   const handleScroll = (sectionId) => {
     if(isOpen) toggleMenu();
     document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
   };
 
   return (
-    <>
       <div
-        className={`mobile-menu ${isOpen ? "active" : ""}`}
-        onClick={toggleMenu}
+          className={`mobile-menu ${isOpen ? "active" : ""}`}
+          onClick={toggleMenu}
       >
         <div className="mobile-menu-container">
-          <img className="logo" src="./assets/images/logo.svg" alt="" />
+          <img className="logo" src="./assets/images/logo.png" alt="Logo" />
 
           <ul>
             <li>
-              <a onClick={() => handleScroll("hero")} className="menu-item">
+              <a
+                  onClick={() => handleScroll("hero")}
+                  className="menu-item"
+                  role="button"
+                  tabIndex="0"
+              >
                 Home
               </a>
             </li>
 
             <li>
-              <a onClick={() => handleScroll("skills")} className="menu-item">
+              <a
+                  onClick={() => handleScroll("skills")}
+                  className="menu-item"
+                  role="button"
+                  tabIndex="0"
+              >
                 Skills
               </a>
             </li>
 
             <li>
-              <a onClick={() => handleScroll("work-exp")} className="menu-item">
+              <a
+                  onClick={() => handleScroll("work-exp")}
+                  className="menu-item"
+                  role="button"
+                  tabIndex="0"
+              >
                 Work Experience
               </a>
             </li>
 
             <li>
-              <a onClick={() => handleScroll("contact")} className="menu-item">
+              <a
+                  onClick={() => handleScroll("contact")}
+                  className="menu-item"
+                  role="button"
+                  tabIndex="0"
+              >
                 Contact Me
               </a>
             </li>
@@ -48,7 +67,6 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
           </ul>
         </div>
       </div>
-    </>
   );
 };
 
